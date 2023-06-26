a67fff91a918f5cd182c5d56741ae9c52c51f5dc
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-06-26 23:04:05 +0000

    Traffic Light exercise June 26,2023

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {TRAFFIC_LIGHT_COLORS } from '../component/home.jsx'
+
+export default function TrafficLight(props) {
+
+    const ()
+
+    return(
+        <div className="traffic-light">
+        
+        </div>
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,16 @@
-import React from "react";
+import React from 'react';
+import TrafficLight from "./TrafficLight.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const TRAFFIC_LIGHT_COLORS = ['red', 'yellow', 'green'];
 
-//create your first component
-const Home = () => {
+const Home = ( => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="">
+
+			<TrafficLight traffickLightColor="red" />
+			<TrafficLight traffickLightColor="yellow" />
+			<TrafficLight traffickLightColor="green" />
+		
 		</div>
 	);
 };
